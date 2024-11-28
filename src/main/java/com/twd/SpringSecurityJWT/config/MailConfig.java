e9920559f97289f6e98bb2a34f500ff400a1d199
DiaeEddineJamal <diae_2002@hotmail.com>
2024-11-28 15:05:31 +0100

    password recvery and reset fully working

--- src/main/java/com/twd/SpringSecurityJWT/config/MailConfig.java
+++ src/main/java/com/twd/SpringSecurityJWT/config/MailConfig.java
@@ -14,8 +14,8 @@ public class MailConfig {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("your-email@gmail.com");
-        mailSender.setPassword("your-app-password");
+        mailSender.setUsername("luziverthegoat@gmail.com");
+        mailSender.setPassword("ozfg csri qnzq ljvo");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
